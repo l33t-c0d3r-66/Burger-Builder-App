aa05e3a4f00bf6846c1fc6bd5ac0b5c7a40b81c4
l33t-c0d3r-66 <aliraza.arain.12530@gmail.com>
2022-06-09 21:36:58 +0500

    Completed Orders Functionality

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,13 +2,38 @@ import React from 'react';
 
 import cssClasses from './Order.css';
 
-const Order = (props) => {
+const order = (props) => {
+
+    const ingredients = [];
+    for(let ingredientName in props.ingredients) {
+        ingredients.push(
+            {
+                name: ingredientName,
+                amount: props.ingredients[ingredientName]
+            });
+    }
+
+    const ingredientOutput = ingredients.map(ingredient => {
+        return <span style={
+            {
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px',
+
+            }
+    }
+        key={ingredient.name}>{ingredient.name} ({ingredient.amount})</span> 
+    })
+
+
     return (
         <div className={cssClasses.Order}>
-            <p>Ingredients: Salad(1)</p>
-            <p> Price: <strong>USD 5.00 </strong> </p>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p> Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)} </strong> </p>
         </div>
     );
 }
 
-export default Order;
+export default order;
